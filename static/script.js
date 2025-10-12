b53f7fadee767636ceafc2db2007edb771bd6aec
JoubertColin <joubert.colin@yahoo.com.br>
2025-10-12 13:54:42 -0300

    Update: style.css, componentes, aula.01.

--- static/script.js
+++ static/script.js
@@ -1,23 +1,46 @@
-console.log("JavaScript carregado!");
-
-// Transformar o texto em áudio
+/*
+    ### Convert Text to Audio ###
+                                    */
 
 const imagens = document.querySelectorAll('.img-audio');
-let audioAtual = null; // guarda o áudio que está tocando
+let audioAtual = null; // Saves the audio that is playing.
 
 imagens.forEach(imagem => {
   imagem.addEventListener('click', () => {
     const srcAudio = imagem.getAttribute('data-audio');
 
-    // Se já existe áudio tocando
+    // If there is already audio playing...
     if (audioAtual && !audioAtual.paused) {
-      audioAtual.pause(); // pausa
-      audioAtual.currentTime = 0; // opcional: reinicia do início
+      audioAtual.pause(); // pause
+      audioAtual.currentTime = 0; // Optional: restart from the beginning.
       audioAtual = null;
     } else {
-      // Cria e toca o novo áudio
+      // Create and play new audio
       audioAtual = new Audio(srcAudio);
       audioAtual.play();
     }
   });
 });
+
+
+
+/*
+    ### Questionnaire with Answer Combination. ###
+                                                    */
+
+function checkAnswer(idInput) {
+  const input = document.getElementById(idInput);
+  const right = input.dataset.correct.trim().toLowerCase();
+  const answer = input.value.trim().toLowerCase();
+  const feedback = document.getElementById("fb" + idInput.replace("resp", ""));
+
+  if (answer === right && answer !== "") {
+    feedback.textContent = "✔";
+    feedback.className = "feedback right";
+  } else {
+    feedback.textContent = "✘";
+    feedback.className = "feedback wrong";
+  }
+}
+
+
